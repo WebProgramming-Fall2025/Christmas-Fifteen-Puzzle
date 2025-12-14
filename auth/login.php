77e03896f5f3152cdf06ac5b2fcf1a4594c1273e
SophieNguyen113 <sophiengocnguyen113@gmail.com>
2025-12-14 03:52:18 -0500

    Updated titles

--- auth/login.php
+++ auth/login.php
@@ -12,7 +12,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/style2.css">
-  <title>User Log In</title>
+  <title>Christmas Fifteen Puzzle - Login</title>
 </head>
 
 <body>
--- auth/user_registration.php
+++ auth/user_registration.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/style2.css">
   <script src = "validation.js"> </script> 
-  <title>User Registration</title>
+  <title>Christmas Fifteen Puzzle - Registration</title>
 </head>
 
 <body>
